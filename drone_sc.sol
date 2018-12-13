e1d1c078e7c02beff7e1052c9558b6d1bff15818
Fran Casino <35731320+francasino@users.noreply.github.com>
2018-12-13 12:21:16 +0200

    Update drone_sc.sol

--- drone_sc.sol
+++ drone_sc.sol
@@ -136,7 +136,7 @@ contract Drone_logistics{
     // only customer can check it 
     // customer will loop outside for this, getting the number of products before with getNumberOfProducts
     function getProduct (uint _productId) public view returns (Product) {
-    	require(msg.sender==customer);
+    	require(msg.sender==vendor || msg.sender==customer);
     	require(_productId > 0 && _productId <= productsCount); 
 
     	return products[_productId];
